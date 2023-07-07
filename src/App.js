b767dea553086b04c214901625e9cb6dcdcd5fb0
a.mahagaonkar@bm-systems.in <a.mahagaonkar@bm-systems.in>
2023-07-07 15:20:17 +0530

    update_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Content from "./components/Content";
 
 function App() {
     return (
         <>
-            <>Yo</>
+            <Content />
         </>
     );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Content() {
+    return (
+        <div style={{ margin: "20px" }}>
+            <img src="/logo192.png" alt="Logo" style={{ width: "40px" }} />
+            <h2>Fun facts about React</h2>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>
+                    Powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Content;
